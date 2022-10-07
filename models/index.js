492018e07d3e58bf50229b11ec080b78f5b3472f
boy-mom <100622402+xoerycaxo@users.noreply.github.com>
2022-10-06 20:24:32 -0400

    set p model files

---
+++ models/index.js
@@ -0,0 +1,38 @@
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+  
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+module.exports = { User, Post, Comment };
